5f0e66acf0d77090ebcee74c973d485ed9ff29dc
boevoivertolet <bjha78rus@yandex.ru>
2022-07-28 15:35:59 +0300

    add styles, fix type setMaxValue

---
+++ src/Counter.tsx
@@ -0,0 +1,49 @@
+import React, {ChangeEvent, useState} from 'react';
+
+export const Counter = () => {
+    let initialValue = 0;
+    let initialMaxValue = 0;
+
+    const [maxValue, setMaxValue] = useState<number>(initialMaxValue)
+    const [value, setValue] = useState<number>(initialValue)
+
+    const toZero = () => {
+        setValue(0)
+    }
+    const minusOneFN = () => {
+        setValue(value - 1)
+    }
+    const plusOneFN = () => {
+        if (value < maxValue)
+            setValue(value + 1)
+    }
+    const fixMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+        let maxValue = Number(e.currentTarget.value)
+        setMaxValue(maxValue)
+    }
+
+
+    return (
+        <div>
+            <div>
+                <button   disabled={value === maxValue} onClick={() => {
+                    plusOneFN()
+                }}>+1
+                </button>
+                <button disabled={value === 0} onClick={() => {
+                    minusOneFN()
+                }}>-1
+                </button>
+                <button disabled={value === 0} onClick={() => {
+                    toZero()
+                }}>reset
+                </button>
+                <div className={value === maxValue ? 'red': 'table'} >{value}</div>
+            </div>
+            <div>
+                <input onChange={fixMaxValue} type="number"/>
+            </div>
+        </div>
+    )
+
+}
